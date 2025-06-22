1b487df7084a41bb6a20b555000b3e4a1cbd81a3
Julian Xhokaxhiu <info@julianxhokaxhiu.com>
2025-06-22 12:48:10 +0200

    Gardening: Use the new .snupkg format

--- .github/workflows/package.ps1
+++ .github/workflows/package.ps1
@@ -1,3 +1,4 @@
 mkdir .dist | Out-Null
 
-Copy-Item ".\${env:_RELEASE_NAME}\bin\${env:_RELEASE_CONFIGURATION}\*.nupkg" -Destination ".\.dist\"
+Move-Item ".\${env:_RELEASE_NAME}\bin\${env:_RELEASE_CONFIGURATION}\*.nupkg" -Destination ".\.dist\"
+Move-Item ".\${env:_RELEASE_NAME}\bin\${env:_RELEASE_CONFIGURATION}\*.snupkg" -Destination ".\.dist\"
