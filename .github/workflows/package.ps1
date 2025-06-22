8eba7885abea667c9e7818e08dc25c50b7c531cf
Julian Xhokaxhiu <info@julianxhokaxhiu.com>
2025-06-22 12:40:59 +0200

    Gardening: Fix two other nuget warnings
    - Fix missing symbols
    - Fix deterministic builds

--- .github/workflows/package.ps1
+++ .github/workflows/package.ps1
@@ -1,3 +1,3 @@
 mkdir .dist | Out-Null
 
-Copy-Item ".\${env:_RELEASE_NAME}\bin\${env:_RELEASE_CONFIGURATION}\*.nupkg" -Destination ".\.dist\${env:_RELEASE_NAME}-${env:_RELEASE_VERSION}_${env:_RELEASE_CONFIGURATION}.nupkg"
+Copy-Item ".\${env:_RELEASE_NAME}\bin\${env:_RELEASE_CONFIGURATION}\*.nupkg" -Destination ".\.dist\"
